e89fa0e512d34bbf777a18b2c13dc2a87bfd7a42
yumaignachante <109954048+yumaignachante@users.noreply.github.com>
2024-07-18 14:21:49 +0700

    fix model

--- models.py
+++ models.py
@@ -3,14 +3,12 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 
 from database import Base
 
-class Student(Base):
-    __tablename__ = 'student'
+class Book(Base):
+    __tablename__ = 'books'
 
     id = Column(Integer, primary_key=True, index=True)
-    firstname = Column(String, index=True)
-    lastname = Column(String, index=True)
-    stu_id = Column(Integer, index=True)
-    dob = Column(String, index=True)
-    sex = Column(String, index=True)
-    age = Column(Integer, index=True)
+    title = Column(String, index=True)
+    author = Column(String, index=True)
+    year = Column(Integer, index=True)
+    is_published = Column(Boolean, index=True)
 
