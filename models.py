1bba5ddc04eb585abe648c990056f1aaeec49e40
yumaignachante <109954048+yumaignachante@users.noreply.github.com>
2024-07-18 14:23:23 +0700

    Update models.py

--- models.py
+++ models.py
@@ -3,6 +3,17 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 
 from database import Base
 
+class Student(Base):
+    __tablename__ = 'student'
+
+    id = Column(Integer, primary_key=True, index=True)
+    firstname = Column(String, index=True)
+    lastname = Column(String, index=True)
+    stu_id = Column(Integer, index=True)
+    dob = Column(String, index=True)
+    sex = Column(String, index=True)
+    age = Column(Integer, index=True)
+    
 class Book(Base):
     __tablename__ = 'books'
 
