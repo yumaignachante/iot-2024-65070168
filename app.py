20ff1e60bbf9ab9bab23456ea732b41edabca2f5
yumaignachante <109954048+yumaignachante@users.noreply.github.com>
2024-07-29 00:04:20 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()
-
-from fastapi import FastAPI, Depends, Response, APIRouter
+from typing import List
+from fastapi import FastAPI, Depends, Response, APIRouter, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
@@ -33,6 +33,8 @@ app.add_middleware(
 
 # https://fastapi.tiangolo.com/tutorial/sql-databases/#crud-utils
 
+
+#book
 @router_v1.get('/books')
 async def get_books(db: Session = Depends(get_db)):
     return db.query(models.Book).all()
@@ -44,7 +46,7 @@ async def get_book(book_id: int, db: Session = Depends(get_db)):
 @router_v1.post('/books')
 async def create_book(book: dict, response: Response, db: Session = Depends(get_db)):
     # TODO: Add validation
-    newbook = models.Book(title=book['title'], author=book['author'], year=book['year'], is_published=book['is_published'])
+    newbook = models.Book(title=book['title'], author=book['author'], year=book['year'], is_published=book['is_published'], description=book['description'], synopsis=book['synopsis'], category=book['category'])
     db.add(newbook)
     db.commit()
     db.refresh(newbook)
@@ -58,6 +60,9 @@ async def update_book(book_id: int, book: dict, db: Session = Depends(get_db)):
     update_book.author = book['author']
     update_book.year = book['year']
     update_book.is_published = book['is_published']
+    update_book.description = book['description']
+    update_book.synopsis = book['synopsis']
+    update_book.category = book['category']
     db.commit()
     db.refresh(update_book)
     Response.status_code = 200
@@ -71,8 +76,6 @@ async def delete_book(book_id: int, response: Response, db: Session = Depends(ge
     response.status_code = 204
     return
 
-
-
 # @router_v1.patch('/books/{book_id}')
 # async def update_book(book_id: int, book: dict, db: Session = Depends(get_db)):
 #     pass
@@ -81,6 +84,9 @@ async def delete_book(book_id: int, response: Response, db: Session = Depends(ge
 # async def delete_book(book_id: int, db: Session = Depends(get_db)):
 #     pass
 
+
+
+#students
 @router_v1.get('/students')
 async def get_students(db: Session = Depends(get_db)):
     return db.query(models.Student).all()
@@ -118,6 +124,140 @@ async def delete_student(student_id: int, response: Response, db: Session = Depe
     db.commit()
     response.status_code = 204
     return
+
+
+#cafe
+@router_v1.get('/menu')
+async def get_menu(db: Session = Depends(get_db)):
+    return db.query(models.Menu).all()
+
+@router_v1.get('/menu/{menu_id}')
+async def get_menu(menu_id: int, db: Session = Depends(get_db)):
+    return db.query(models.Menu).filter(models.Menu.id == menu_id).first()
+
+@router_v1.post('/menu')
+async def create_menu(menu: dict, response: Response, db: Session = Depends(get_db)):
+    # TODO: Add validation
+    newmenu = models.Menu(name=menu['name'], price=menu['price'], description=menu['description'])
+    db.add(newmenu)
+    db.commit()
+    db.refresh(newmenu)
+    response.status_code = 201
+    return newmenu
+
+@router_v1.patch('/menu/{menu_id}')
+async def update_menu(menu_id: int, menu: dict, db: Session = Depends(get_db)):
+    update_menu = db.query(models.Menu).filter(models.Menu.id == menu_id).first()
+    update_menu.name = menu['name']
+    update_menu.price = menu['price']
+    update_menu.description = menu['description']
+    db.commit()
+    db.refresh(update_menu)
+    Response.status_code = 200
+    return update_menu
+
+@router_v1.delete('/menu/{menu_id}')
+async def delete_menu(menu_id: int, response: Response, db: Session = Depends(get_db)):
+    menu = db.query(models.Menu).filter(models.Menu.id == menu_id).first()
+    db.delete(menu)
+    db.commit()
+    response.status_code = 204
+    return
+
+
+# order
+@router_v1.get("/order/{order_id}")
+def read_order(order_id: int, db: Session = Depends(get_db)):
+    db_order = (
+        db.query(models.Order)
+        .filter(models.Order.id == order_id)
+        .first()
+
+    )
+
+    if db_order is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+    db_order.menu=db.query(models.Menu).filter(models.Menu.id== db_order.menu_id).first()
+    return {
+        "id":db_order.id,
+        "menu_id":db_order.menu_id,
+        "quantity":db_order.quantity,
+        "status":db_order.status,
+        "note":db_order.note,
+        "menu_name":db_order.menu.name,
+    }
+
+
+@router_v1.get("/order")
+def get_order(db: Session = Depends(get_db)):
+    order = db.query(models.Order).join(models.Menu).all()
+    order_responses = []
+    for order in order:
+        order_responses.append(
+            {
+                "id":order.id,
+                "menu_id":order.menu_id,
+                "quantity":order.quantity,
+                "status":order.status,
+                "note":order.note,
+                "menu_name":order.menu.name
+            }
+        )
+    return order_responses
+
+
+@router_v1.post("/order")
+def create_order(
+    order: models.OrderCreate, response: Response, db: Session = Depends(get_db)
+):
+    print(order)
+    db_coffee = (
+        db.query(models.Menu).filter(models.Menu.id == order.menu_id).first()
+    )
+    if db_coffee is None:
+        raise HTTPException(status_code=404, detail="Menu not found")
+
+    db_order = models.Order(
+        menu_id=order.menu_id, quantity=order.quantity, note=order.note
+    )
+    db.add(db_order)
+    db.commit()
+    db.refresh(db_order)
+    response.status_code = 200
+    return db_order
+
+
+@router_v1.patch("/order/{order_id}")
+async def patch_order(
+    order_id: int,
+    order: models.OrderUpdate,
+    db: Session = Depends(get_db),
+):
+    print(order)
+    db_order = db.query(models.Order).filter(models.Order.id == order_id).first()
+    if db_order is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+
+    order_data = order.dict(exclude_unset=True)
+    for key, value in order_data.items():
+        setattr(db_order, key, value)
+
+    db.commit()
+    db.refresh(db_order)
+
+    return db_order
+
+
+@router_v1.delete("/order/{order_id}")
+async def delete_order(order_id: int, db: Session = Depends(get_db)):
+    order = db.query(models.Order).filter(models.Order.id == order_id).first()
+
+    if order is None:
+        raise HTTPException(status_code=404, detail="Order not found")
+
+    db.delete(order)
+    db.commit()
+    return {"detail": "Order deleted"}
 
 app.include_router(router_v1)
 
--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-# from sqlalchemy.orm import relationship
-
+from sqlalchemy.orm import relationship
+from typing import Optional
 from database import Base
+from pydantic import BaseModel
 
 class Student(Base):
     __tablename__ = 'student'
@@ -22,4 +23,44 @@ class Book(Base):
     author = Column(String, index=True)
     year = Column(Integer, index=True)
     is_published = Column(Boolean, index=True)
+    description = Column(String, index=True)
+    synopsis = Column(String, index=True)
+    category = Column(String, index=True)
 
+class Menu(Base):
+    __tablename__ = 'menu'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    price = Column(Integer, index=True)
+    description = Column(String, index=True)
+
+class Order(Base):
+    __tablename__ = 'order'
+
+    id = Column(Integer, primary_key=True, index=True)
+    menu_id = Column(Integer, ForeignKey("menu.id", ondelete="CASCADE"), index=True)
+    menu = relationship("Menu")
+    quantity = Column(Integer, index=True)
+    status = Column(String, default="pending", index=True)
+    note = Column(String, index=True)
+
+class OrderResponse(BaseModel):
+    id: int
+    menu_id: int
+    quantity: int
+    status: str
+    note: str
+    menu_name: str
+
+
+class OrderCreate(BaseModel):
+    menu_id: int
+    quantity: int
+    note: str = None
+
+class OrderUpdate(BaseModel):
+    menu_id: Optional[int] = None
+    quantity: Optional[int] = None
+    note: Optional[str] = None
+    status: Optional[str] = None
